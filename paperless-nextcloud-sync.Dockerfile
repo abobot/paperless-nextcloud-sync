b1fd9e8aa40063bb6fea4fe65386a6d715cb6143
Flo-R1der <findigutt-github@yahoo.com>
2024-12-26 21:51:38 +0100

    filewatcher implemented, actions are now based on events

--- paperless-nextcloud-sync.Dockerfile
+++ paperless-nextcloud-sync.Dockerfile
@@ -1,7 +1,7 @@
 FROM ubuntu:latest
 
 RUN apt-get update \
-  && apt-get install -y davfs2 ca-certificates locales \
+  && apt-get install -y davfs2 ca-certificates locales inotify-tools \
   && mkdir -p /mnt/source \
   && mkdir -p /mnt/compare \
   && mkdir -p /mnt/webdrive \
@@ -14,7 +14,7 @@ ENV LANGUAGE=en_US:de
 ENV LC_ALL=en_US.UTF-8
 
 COPY ./start.sh /usr/local/bin
-COPY ./sync.sh /usr/local/bin
+#COPY ./sync.sh /usr/local/bin
 
 ENTRYPOINT [ "/usr/local/bin/start.sh" ]
 
--- start.sh
+++ start.sh
@@ -23,6 +23,8 @@ FOLDER_USER=${SYNC_USERID:-0}
 FOLDER_GROUP=${SYNC_GROUPID:-0}
 ACCESS_DIR=${SYNC_ACCESS_DIR:-755}
 ACCESS_FILE=${SYNC_ACCESS_FILE:-755}
+SOURCE_DIR="/mnt/source"
+WEBDRIVE_DIR="/mnt/webdrive"
 
 # Set local Variables
 locale-gen $LC_ALL
@@ -38,15 +40,49 @@ if [ -f "/var/run/mount.davfs/mnt-webdrive.pid" ]; then
   rm /var/run/mount.davfs/mnt-webdrive.pid
 fi
 mount -t davfs $WEBDRIVE_URL /mnt/webdrive \
--o uid=$FOLDER_USER,gid=$FOLDER_GROUP,dir_mode=$ACCESS_DIR,file_mode=$ACCESS_FILE
+  -o uid=$FOLDER_USER,gid=$FOLDER_GROUP,dir_mode=$ACCESS_DIR,file_mode=$ACCESS_FILE
 
 
 
-echo "Start completed, starting sync-loop"
-echo "============================================================================="
+echo "[INFO] Start completed. Start filewatcher"
+echo "===================================================================================================="
 
-while true; do 
-  /bin/bash /usr/local/bin/sync.sh
-  echo "-----------------------------------------------------------------------------"
-  sleep 30s
+
+
+# setting up filewatcher and actions for for high-performance instant synchronization per-event
+# supports renaming and file-move, to preserve existing files in nextcloud (instead of delete+recreate)
+inotifywait -m -r -e modify,create,delete,move "$SOURCE_DIR" --format '%e|%w%f|%f' |
+while IFS='|' read -r event full_path filename; do
+  RELATIVE_PATH="${full_path/${SOURCE_DIR}/''}"
+  case "$event" in
+    MODIFY|CREATE)
+      echo "[ACTION] Detected $event-Event - Copying: $filename"
+      cp "$SOURCE_DIR/$RELATIVE_PATH" "$WEBDRIVE_DIR/$RELATIVE_PATH" --verbose
+      ;;
+    DELETE)
+      echo "[ACTION] Detected $event-Event - Deleting: $filename"
+      rm "$WEBDRIVE_DIR/$RELATIVE_PATH" --verbose
+      ;;
+    MOVED_FROM)
+      echo "[INFO] Detected $event-Event - File moved: $filename"
+      #OLD_PATH_LOCAL="$SOURCE_DIR/$RELATIVE_PATH"
+      OLD_PATH_WEBDRIVE="$WEBDRIVE_DIR/$RELATIVE_PATH"
+      ;;
+    MOVED_TO)
+      echo "[ACTION] Detected $event-Event - File moved: $filename"
+      #NEW_PATH_LOCAL="$SOURCE_DIR/$RELATIVE_PATH"
+      NEW_PATH_WEBDRIVE="$WEBDRIVE_DIR/$RELATIVE_PATH"
+      if [[ -n "$OLD_PATH_WEBDRIVE" ]]; then
+        mv "$OLD_PATH_WEBDRIVE" "$NEW_PATH_WEBDRIVE" --verbose
+        #NEW_PATH_LOCAL=""
+        NEW_PATH_WEBDRIVE=""
+      else
+        echo "[ERROR] Variable \"OLD_PATH_WEBDRIVE\" not set"
+      fi
+      ;;
+    *)
+      echo "[ERROR] Unknown $event-Event for $filename"
+      echo "Full path: $full_path"
+      ;;
+  esac
 done
