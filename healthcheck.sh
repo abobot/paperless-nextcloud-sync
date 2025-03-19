f2e1027047633589ab433fd63b5769df81c208b6
阿波a-bo <32825442+abobot@users.noreply.github.com>
2025-03-20 00:34:03 +0800

    Update healthcheck.sh

--- healthcheck.sh
+++ healthcheck.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
-# Check if Webdrive is mounted
-if mount | grep -q "/mnt/webdrive"; then
-  echo "WebDAV is mounted"
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+# 检查 WebDAV 挂载状态
+if findmnt -r /mnt/webdrive > /dev/null; then
+  echo -e "${GREEN}[INFO] WebDAV is mounted.${NC}"
 else
-  echo "[ERROR] WebDAV is not mounted."
+  echo -e "${RED}[ERROR] WebDAV is not mounted!${NC}"
   exit 1
 fi
-echo " | "
-# Check if `inotifywait` is still running
-if pgrep -x "inotifywait" > /dev/null; then
-  echo "Filewatcher is running"
+
+# 检查 `inotifywait` 进程是否在运行
+if pgrep -f "inotifywait" > /dev/null; then
+  echo -e "${GREEN}[INFO] Filewatcher is running.${NC}"
 else
-  echo "[ERROR] Filewatcher is not running"
+  echo -e "${RED}[ERROR] Filewatcher is not running!${NC}"
   exit 1
 fi
 
