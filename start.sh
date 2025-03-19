2c4c76d83577542fee0fc08c55654a06d12000a3
阿波a-bo <32825442+abobot@users.noreply.github.com>
2025-03-20 00:30:47 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,133 +1,64 @@
 #!/bin/bash
 
-# Set local Variables for full UTF-8 support
-if [[ $LC_ALL != "en_US.UTF-8" ]]; then
-  locale-gen "${LC_ALL}"
-fi
+# Set locale
+export LC_ALL=en_US.UTF-8
 
-# Check mandatory variables and store them to secrets file
-if [ -z ${WEBDRIVE_USER} ]; then
-  echo "[ERROR] Webdrive user is not set!"
+# Check variables
+if [[ -z "${WEBDRIVE_USER}" || -z "${WEBDRIVE_URL}" ]]; then
+  echo "[ERROR] Missing WEBDRIVE_USER or WEBDRIVE_URL"
   exit 1
 fi
 
-if [ -n "${WEBDRIVE_PASSWORD_FILE}" ]; then
-    WEBDRIVE_PASSWORD=$(read ${WEBDRIVE_PASSWORD_FILE})
+if [[ -n "${WEBDRIVE_PASSWORD_FILE}" ]]; then
+    WEBDRIVE_PASSWORD=$(cat "${WEBDRIVE_PASSWORD_FILE}")
 fi
-if [ -z ${WEBDRIVE_PASSWORD} ]; then
-  echo "[ERROR] Webdrive password is not set!"
+if [[ -z "${WEBDRIVE_PASSWORD}" ]]; then
+    echo "[ERROR] WEBDRIVE_PASSWORD not set!"
     exit 1
-fi
-
-if [ -z ${WEBDRIVE_URL} ]; then
-  echo "[ERROR] Webdrive url is not set!"
-  exit 1
 fi
 
 echo "$WEBDRIVE_URL $WEBDRIVE_USER $WEBDRIVE_PASSWORD" > /etc/davfs2/secrets
 
-# Set optional variables
-DIR_USER=${SYNC_USERID:-0}
-DIR_GROUP=${SYNC_GROUPID:-0}
-ACCESS_DIR=${SYNC_ACCESS_DIR:-755}
-ACCESS_FILE=${SYNC_ACCESS_FILE:-755}
-SOURCE_DIR="/mnt/source"
-WEBDRIVE_DIR="/mnt/webdrive"
-
-# Create user
-if [ $DIR_USER -gt 0 ]; then
-  useradd webdrive -u $DIR_USER -N -G $DIR_GROUP
-fi
-
-# Mount the webdav drive 
-echo "[INFO] WEBDRIVE_URL: $WEBDRIVE_URL"
-echo "[INFO] WEBDRIVE_USER: $WEBDRIVE_USER"
-if [ -f "/var/run/mount.davfs/mnt-webdrive.pid" ]; then
-  rm /var/run/mount.davfs/mnt-webdrive.pid
-fi
-mount -t davfs "$WEBDRIVE_URL" /mnt/webdrive \
-  -o uid="$DIR_USER",gid="$DIR_GROUP",dir_mode="$ACCESS_DIR",file_mode="$ACCESS_FILE"
-if [ $? -ne 0 ]; then
+# Mount WebDAV
+mount -t davfs "$WEBDRIVE_URL" /mnt/webdrive -o uid=0,gid=0,dir_mode=755,file_mode=755
+if [[ $? -ne 0 ]]; then
   echo "[ERROR] Failed to mount $WEBDRIVE_URL"
-  echo "[ERROR] Please check your credentials or URL."
   exit 1
 fi
 
+echo "[INFO] Starting sync..."
+/bin/bash sync.sh "/mnt/source" "/mnt/webdrive" "initial-sync" &
 
-# Trap signals (SIGTERM, SIGINT) and pass them to child processes
-function container_exit() {
-  SIGNAL=$1
-  echo "[WARNING] Received $SIGNAL, ending processes..."
-  while $(kill -$SIGNAL $(jobs -p) 2>/dev/null); do
-    sleep 3
-  done
-  wait
-}
-trap "container_exit SIGTERM" SIGTERM
-trap "container_exit SIGINT" SIGINT
-
-
-echo "[INFO] Start completed. Start initial synchronization and file watcher"
-echo "===================================================================================================="
-
-
-# start the initial synchronization as background job
-# this script prints output in container logs, when finished
-# usage sync.sh: $1 = source | $2 = destination | $3 = reason
-/bin/bash sync.sh "$SOURCE_DIR" "$WEBDRIVE_DIR" "container-start" &
-
-
-# setting up file watcher and actions for for high-performance instant synchronization per-event
-# supports renaming and file-move, to preserve existing files in Nextcloud (instead of delete+recreate)
-inotifywait -m -r -e modify,create,delete,move "$SOURCE_DIR" --format '%e|%w%f|%f' |
-while IFS='|' read -r event full_path filename; do
-  RELATIVE_PATH="${full_path/${SOURCE_DIR}\//''}"
+# Watch for changes using inotify
+inotifywait -m -r -e modify,create,delete,move --exclude '.*\.swp|.*\.tmp' "/mnt/source" --format '%e|%w%f' |
+while IFS='|' read -r event full_path; do
+  RELATIVE_PATH="${full_path#/mnt/source/}"
   case "$event" in
     MODIFY|CREATE)
-      echo "[ACTION] Detected $event-Event - Copying file: $filename"
-      cp "$SOURCE_DIR/$RELATIVE_PATH" "$WEBDRIVE_DIR/$RELATIVE_PATH" --verbose
+      rsync -av "$full_path" "/mnt/webdrive/$RELATIVE_PATH"
       ;;
     DELETE)
-      echo "[ACTION] Detected $event-Event - Deleting file: $filename"
-      rm "$WEBDRIVE_DIR/$RELATIVE_PATH" --verbose
+      rm -f "/mnt/webdrive/$RELATIVE_PATH"
       ;;
     CREATE,ISDIR)
-      echo "[ACTION] Detected $event-Event - Creating directory: $RELATIVE_PATH"
-      mkdir "$WEBDRIVE_DIR/$RELATIVE_PATH" --verbose
+      mkdir -p "/mnt/webdrive/$RELATIVE_PATH"
       ;;
     DELETE,ISDIR)
-      echo "[ACTION] Detected $event-Event - Deleting directory: $RELATIVE_PATH"
-      rm -d "$WEBDRIVE_DIR/$RELATIVE_PATH" --verbose
+      rm -rf "/mnt/webdrive/$RELATIVE_PATH"
       ;;
     MOVED_FROM)
-      echo "[INFO] Detected $event-Event - File moved: $RELATIVE_PATH"
-      OLD_PATH_WEBDRIVE="$WEBDRIVE_DIR/$RELATIVE_PATH"
+      OLD_PATH_WEBDRIVE="/mnt/webdrive/$RELATIVE_PATH"
       ;;
     MOVED_TO)
-      echo "[ACTION] Detected $event-Event - Moving file: $RELATIVE_PATH"
-      NEW_PATH_WEBDRIVE="$WEBDRIVE_DIR/$RELATIVE_PATH"
-      if [[ -n "$OLD_PATH_WEBDRIVE" && -f "$OLD_PATH_WEBDRIVE" ]]; then
-          mv "$OLD_PATH_WEBDRIVE" "$NEW_PATH_WEBDRIVE" --verbose
+      NEW_PATH_WEBDRIVE="/mnt/webdrive/$RELATIVE_PATH"
+      if [[ -n "$OLD_PATH_WEBDRIVE" && -e "$OLD_PATH_WEBDRIVE" ]]; then
+          mv "$OLD_PATH_WEBDRIVE" "$NEW_PATH_WEBDRIVE"
       else
-        if [[ ! -n "$OLD_PATH_WEBDRIVE" ]]; then
-          echo "[WARNING] Variable \"OLD_PATH_WEBDRIVE\" not set! Copying as new file!"
-        fi
-        if [[ ! -f "$OLD_PATH_WEBDRIVE" ]]; then
-          echo "[WARNING] File from MOVED_FROM event does not exist! Copying as new file!"
-        fi
-        cp "$SOURCE_DIR/$RELATIVE_PATH" "$WEBDRIVE_DIR/$RELATIVE_PATH" --verbose
-        echo "[INFO] Start complete sync run to fix other potential failures"
-        /bin/bash sync.sh "$SOURCE_DIR" "$WEBDRIVE_DIR" "missed-events" &
-        # usage sync.sh: $1 = source | $2 = destination | $3 = reason
+          rsync -av "$full_path" "/mnt/webdrive/$RELATIVE_PATH"
       fi
       unset OLD_PATH_WEBDRIVE
-      unset NEW_PATH_WEBDRIVE
-      ;;
-    *)
-      echo "[ERROR] Unknown $event-Event for $filename"
-      echo "Full path: $full_path"
       ;;
   esac
-  unset RELATIVE_PATH
 done &
+
 wait
